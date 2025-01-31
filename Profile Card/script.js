4bf72a78c4bee7db883f6ec0793d08921b158625
Benedict Ogbonna <137557100+optimistsound@users.noreply.github.com>
2025-01-31 20:50:16 +0100

    Delete Profile Card directory

--- Profile Card/script.js
+++
@@ -1,9 +0,0 @@
-const utcTimeElement = document.getElementById("utc-time");
-
-function updateTime() {
-  const now = new Date();
-  utcTimeElement.textContent = now.toUTCString();
-}
-
-updateTime();
-setInterval(updateTime, 1000);
